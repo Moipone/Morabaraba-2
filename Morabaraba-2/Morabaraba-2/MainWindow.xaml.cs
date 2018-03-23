f39464feff2970aefa6c6b2e82b8fc3e39f0e53a
MoiponeT <Moipone.Tsaoane@gmail.com>
2018-03-23 14:11:08 +0200

    Create variables for interactive components

--- Morabaraba-2/Morabaraba-2/MainWindow.xaml.cs
+++ Morabaraba-2/Morabaraba-2/MainWindow.xaml.cs
@@ -20,10 +20,19 @@ namespace Morabaraba_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        //global variables 
+        int p1Cows = 12; //initial number of cows for each player 
+        int p2Cows = 12;
+        SolidColorBrush cowcolor = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        //Each of the characterd represent a space on the game board, and will turn Yellow or Blue depending on the player value
+        char A1, A4, A7, B2, B4, B6, C3, C4, C5, D1, D2, D3, D5, D6, D7, E3, E4, E5, F2, F4, F6, G1, G4, G7 = 'B';
+        char playerY = 'Y';
+        char playerB = 'B';
+
         public MainWindow()
         {
             InitializeComponent();
-         
+            
 
         }
     }
